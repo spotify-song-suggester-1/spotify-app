6e8ccf8022df366a239e158949e76ff94cb8d508
Andrei Lysenko <andrei.lysenko@outlook.com>
2020-02-27 19:06:56 -0800

    Syncing Up Files,

---
+++ web/spotify-web/src/components/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LandingPage = () =>{
+    return(
+        <>
+        <div><h1>NAV BAR PLACE HOLDER</h1></div>
+        <button>Sign Up</button>
+        <button>Log In</button>
+        </>
+    )
+}
+export default LandingPage;
--- web/spotify-web/src/styles/styles.js
+++ web/spotify-web/src/styles/styles.js
@@ -1,1 +1,23 @@
-wohoo
+import styled from "styled-components";
+
+//  Card Components
+
+export const CardBase = styled.div`
+  border-radius: 8px;
+  background-color: white;
+  margin: ${props => props.divMargin};
+  height: ${props => props.cardHeight};
+  width: ${props => props.cardWidth};
+`;
+
+// Alignment
+
+export const VerticalAlign = styled.div`
+  display: flex;
+  flex-flow: column;
+  height: ${props => props.contentHeight};
+`;
+
+export const HorizontalAlign = styled.div`
+  display: flex;
+`;
